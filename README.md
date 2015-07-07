b5b9b17ec174427fdb9318a0f2eee8d4a64fdc3d
Diego <diegobiagiviana@gmail.com>
2015-07-07 14:01:00 -0300

    Readme atualizado.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# site
+# PHP Site
 Site simples e funcional usando PHP, MySql, HTML5 e Javascript.
+
+## Instruções para instalação
+- Execute o arquivo *dump.sql*.
+- Arrume o arquivo de variáveis globais, *globals.php*, de acordo com seu ambiente.
--- globals.php
+++ globals.php
@@ -3,4 +3,4 @@ define('SITE_TITLE', 'Site Funcional em PHP');
 define('PROJ_NAME', 'site/');
 define('SITE_URL', $_SERVER['HTTP_HOST'] . '/' . PROJ_NAME);
 define('SITE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/' . PROJ_NAME);
-define('ADMIN_EMAIL', 'diegobiagiviana@gmail.com');
+define('ADMIN_EMAIL', 'admin@admin.com');
